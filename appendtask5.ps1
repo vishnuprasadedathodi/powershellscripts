5d7158203ebd9378d745999d344da851bbec1cb1
vishnuprasadedathodi <83941322+vishnuprasadedathodi@users.noreply.github.com>
2021-10-06 17:56:54 +0530

    task5 added

---
+++ appendtask5.ps1
@@ -0,0 +1,23 @@
+﻿$list=Get-ChildItem -Path D:\powershell_basics\prudentialTraining
+Write-Host $list 
+
+$ps1list=Get-ChildItem -Path D:\powershell_basics\prudentialTraining -Recurse|where {$_.extension -eq ".ps1"}
+
+if( Test-Path D:\powershell_basics\append.ps1 )
+{
+Remove-Item D:\powershell_basics\append.ps1
+}
+New-Item -ItemType File -Path D:\powershell_basics -Name append.ps1
+
+function appendfile($path)
+{
+
+foreach($item in $ps1list)
+{
+ $content=Get-Content -Path $item
+ 
+ Add-Content -Path $path -Value $content 
+
+}
+}
+appendfile D:\powershell_basics\append.ps1
